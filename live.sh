2b48d7d6039d17e368a4956094cf9f2a8c9f2991
François Vieille <vieille.francois@gmail.com>
2017-03-21 00:03:45 +0100

    Create two ways

--- live.sh
+++ live.sh
@@ -12,11 +12,11 @@ issLiveIsDown="issLiveIsDown.jpg"
 # get one frame of live stream
 
 ffmpeg -loglevel panic -y -i http://iphone-streaming.ustream.tv/uhls/17074538/streams/live/iphone/playlist.m3u8  -an -q:v 2 -frames:v 1 $issfile
-# ffmpeg -y -i http://iphone-streaming.ustream.tv/uhls/17074538/streams/live/iphone/playlist.m3u8 -map 0:p:1 -an -q:v 2 -frames:v 1 $issfile
 
 failed=$?
 
 # process image
+
 if [ ${failed} -eq "0" ] 
 then
 
@@ -40,25 +40,30 @@ then
 		exit 1;
 	fi
 
-	convert $issfile -filter spline -resize 1920x -unsharp 0x4+0.4+0 $default
-
-	if [[ $1 = "--with-location" ]]
+	if [[ $1 = "--wallpaper" ]]
 	then
+		convert $issfile -filter spline -resize 1920x -unsharp 0x4+0.4+0 $default
+
+		if [[ $2 = "--with-location" ]]
+		then
+			location=`./location.sh`
+			caption="`date +%d/%m/%Y\ -\ %H:%M` - $location"
+			convert $default -gravity northWest \
+	    	      -stroke '#000C' -strokewidth 2 -pointsize 15 -annotate 0 "$caption" \
+	        	  -stroke  none   -fill white    -pointsize 15 -annotate 0 "$caption" \
+	  				$default
+	  	fi
+
+		DISPLAY=:0.0 feh --bg-center $default
+
+	elif [[ $1 = "--post-fb" ]]
+	then
+
 		location=`./location.sh`
-		caption="`date +%d/%m/%Y\ -\ %H:%M` - $location"
-		convert $default -gravity northWest \
-    	      -stroke '#000C' -strokewidth 2 -pointsize 15 -annotate 0 "$caption" \
-        	  -stroke  none   -fill white    -pointsize 15 -annotate 0 "$caption" \
-  				$default
+		access_token_fb=`cat ACCESS_TOKEN`
+	  	id_page_fb=`cat ID_PAGE`
+	  	python ./post_fb.py $access_token_fb $id_page_fb $issfile "$location" >> /dev/null
 
-  		if [[ $2 = "--post-fb" ]]
-  		then
-  			access_token_fb=`cat ACCESS_TOKEN`
-  			id_page_fb=`cat ID_PAGE`
-  			python ./post_fb.py $access_token_fb $id_page_fb $issfile "$location" >> /dev/null
-  		fi
-  	fi
+	fi
 
-
-	DISPLAY=:0.0 feh --bg-center $default
 fi
